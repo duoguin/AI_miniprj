2b24362705a6fcef03b87fcfb2bf6e4f3f514557
Himar <macbook@MacBook-Pro-cua-MACBOOK-3.local>
2026-05-03 23:07:39 +0700

    get api

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_FILE = os.path.join(BASE_DIR, 'db', 'transactions.csv')
+
+# Tạo file CSV với các cột chuẩn nếu file chưa tồn tại
+# def init_db():
+#     if not os.path.exists(DATA_FILE):
+#         os.makedirs(os.path.dirname(DATA_FILE), exist_ok=True)
+#         with open(DATA_FILE, mode='w', encoding='utf-8') as f:
+#             f.write("Date,Description,Amount,Transaction Type,Category,Account Name,Month\n")
+
+# init_db()
